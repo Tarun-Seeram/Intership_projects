401484f80234cffab02921c24b538603bf151d01
Tarun_Seeram <tarunseeram1175@gmail.com>
2024-11-06 22:52:10 +0530

    calculator.java

--- calculator/src/calculator.java
+++ calculator/src/calculator.java
@@ -1,24 +1,19 @@
 class Calculator {
+   public static double add(double a, double b){
+       return a+b;
+   }
+   public static double sub(double a, double b){
+       return a-b;
+   }
+   public static double mul(double a, double b){
+       return a*b;
+   }
+   public static double div(double a, double b) {
+       if(b == 0){
+           throw new IllegalArgumentException("Cannot divided by '0'");
+       }else {
+           return a / b;
+       }
+   }
 
-    public double add(double a, double b) {
-        return a + b;
-    }
-
-
-    public double subtract(double a, double b) {
-        return a - b;
-    }
-
-
-    public double multiply(double a, double b) {
-        return a * b;
-    }
-
-
-    public double divide(double a, double b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
-        }
-        return a / b;
-    }
 }
